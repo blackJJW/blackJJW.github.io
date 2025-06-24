e8b88c4f1b90deda728bf5cd42490c4cc3441440
blackJJW <jjinwoo92@gmail.com>
2025-06-24 21:33:26 +0900

    rebuilding site Tue Jun 24 21:33:26 KST 2025

---
+++ searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Black_JJW's Blog",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Black_JJW's Blog",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
